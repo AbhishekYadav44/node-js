5fb35a5e0c6c4b30689331cd96a8bfd2f925ab95
abhishek-yadav-2006 <ydvabhishek447@gmail.com>
2026-06-02 14:48:58 +0530

    ES6 module

---
+++ sction-2/ES6_module/app.js
@@ -0,0 +1,5 @@
+import mul   from "./math.js";  //   export default ko aise import krte h bna {}  ke
+import { sum } from "./math.js";  // named export ko with {}   import krte h
+
+console.log(sum(2,3));
+console.log(mul(3,4));
---
+++ sction-2/ES6_module/math.js
@@ -0,0 +1,10 @@
+
+//  names export
+export function sum(a,b){
+     return a+b;
+}
+
+// default report 
+export default function mul(a,b){
+    return a*b;
+}
